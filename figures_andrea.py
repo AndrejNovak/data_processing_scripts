0b8fec6a605c7377c921ba742567121266980ed0
Andrej Novák <novk.andrej@gmail.com>
2023-09-14 10:53:01 +0200

    Update

--- figures_andrea.py
+++ figures_andrea.py
@@ -46,7 +46,7 @@ for idx, var in enumerate(all_paths):
     label_to_fig_name = label[idx]
 
     for k in range(len(E_min)):
-        print(f'Filter for Energy range {E_min[k]}-{E_max[k]} keV and size range {Size_min[k]}-{Size_max[k]} px')
+        #print(f'Filter range for Energy {E_min[k]}-{E_max[k]} keV, size {Size_min[k]}-{Size_max[k]} px')
         filter_parameters = Cluster_filter_multiple_parameter([E_min[k], E_max[k], Size_min[k], Size_max[k]], [4,7]) # Energy, Size
         filtered_elist = read_elist_filter_numpy(elist_data, filter_parameters)
         square_matrices = create_matrix_filter_tpx3_t3pa_for_filtering_numpy_input(filtered_elist[:,-1], clog, number_of_particles)
@@ -67,16 +67,10 @@ for idx, var in enumerate(all_paths):
         #    print_figure_energy(square_matrices[4], energy_colorbar_max_value, 'Failed - ' + label[idx], FolderOut, FileOutName + '_3_failed')
         #except Exception:
         #    pass
-
-
-
-'Q:\\DPE_andrej_data_output\\2023_05_16_TPX2_VdG\\X00\\02_200V\\',
-'C:\\Users\\andrej\\Documents\\FEI\\data_processing_scripts\\figures_andrea\\',
 """
 
-#FolderInPath = 'Q:\\DPE_andrej_data_output\\2023_05_16_TPX2_VdG\\X00\\02_200V\\Files\\'
-FolderInPath = 'Q:\\DPE_andrej_data_output\\2023_05_16_TPX2_VdG\\X00\\04_200V\\Files\\'
 
+FolderInPath = 'Q:\\DPE_andrej_data_output\\2023_05_16_TPX2_VdG\\X00\\02_200V\\Files\\'
 FolderOut = 'C:\\Users\\andrej\\Documents\\FEI\\data_processing_scripts\\figures_andrea\\'
 
 print(FolderInPath)
@@ -92,36 +86,94 @@ if len(elist_data[:,0]) == len(clog[:]):
 else:
     print('Really bad! The Elist and Clog are NOT of the same length', len(elist_data[:,0]), len(clog[:]))
 
-E_min = np.array([100,200,300,400,500])
-E_max = np.array([50000,50000,50000,50000,50000])
-Size_min = np.array([25,50,75,100,150])
-Size_max = np.array([10000,10000,10000,10000,10000])
+E_min = np.array([100,150,200,250,300,350,400,450,500])
+E_max = 500000
+Size_min = np.array([10,20,30,40,50,60,70,80,90,100,125,150])
+Size_max = 1000
+Roundness_min = np.array([0.50,0.60,0.70,0.75,0.80,0.85,0.90,0.95,0.98,0.99])
+Roundness_max = 1.01
 
 number_of_particles = len(elist_data[:,0])
 
 label_to_fig_name = '14.8 MeV neutrons'
-label_to_fig_name = '16.2 MeV neutrons'
+folder_name = '14_8MeV\\'
 
 for k in range(len(E_min)):
     for l in range(len(Size_min)):
-        print(f'Filter for Energy range {E_min[k]}-{E_max[k]} keV and size range {Size_min[l]}-{Size_max[l]} px')
-        filter_parameters = Cluster_filter_multiple_parameter([E_min[k], E_max[k], Size_min[l], Size_max[l]], [4,7]) # Energy, Size
-        filtered_elist = read_elist_filter_numpy(elist_data, filter_parameters)
-        square_matrices = create_matrix_filter_tpx3_t3pa_for_filtering_numpy_input(filtered_elist[:,-1], clog, number_of_particles)
+        for j in range(len(Roundness_min)):
+            print(f'Filter range for Energy {E_min[k]}-{E_max} keV, size {Size_min[l]}-{Size_max} px, roundness {Roundness_min[j]}-{Roundness_max}.')
+            filter_parameters = Cluster_filter_multiple_parameter([E_min[k], E_max, Size_min[l], Size_max, Roundness_min[j], Roundness_max], [4,7,10]) # Energy, Size
+            filtered_elist = read_elist_filter_numpy(elist_data, filter_parameters)
+            square_matrices = create_matrix_filter_tpx3_t3pa_for_filtering_numpy_input(filtered_elist[:,-1], clog, number_of_particles)
 
-        energy_colorbar_max_value = 10000
+            energy_colorbar_max_value = 10000
 
-        FileOutName = str(label_to_fig_name) + '_E_' + str(E_min[k]) + '_Size_' + str(Size_min[l]) 
+            FileOutName = str(label_to_fig_name) + '_E_' + str(E_min[k]) + '_Size_' + str(Size_min[l]) + '_Roundness_' + str(Roundness_min[j])
 
-        #try:
-        #    print_figure_energy(square_matrices[0], energy_colorbar_max_value, 'All - ' + label_to_fig_name, FolderOut, FileOutName + '_1_all')
-        #except Exception:
-        #    pass
-        try:
-            print_figure_energy(square_matrices[2], energy_colorbar_max_value, 'Passed - ' + label_to_fig_name, FolderOut, FileOutName + '_2_passed')
-        except Exception:
-            pass
-        #try:
-        #    print_figure_energy(square_matrices[4], energy_colorbar_max_value, 'Failed - ' + label_to_fig_name, FolderOut, FileOutName + '_3_failed')
-        #except Exception:
-        #    pass
+            #try:
+            #    print_figure_energy(square_matrices[0], energy_colorbar_max_value, 'All - ' + label_to_fig_name, FolderOut, FileOutName + '_1_all')
+            #except Exception:
+            #    pass
+            try:
+                print_figure_energy(square_matrices[2], energy_colorbar_max_value, 'Passed - ' + label_to_fig_name, FolderOut + folder_name, FileOutName + '_2_passed')
+            except Exception:
+                pass
+            #try:
+            #    print_figure_energy(square_matrices[4], energy_colorbar_max_value, 'Failed - ' + label_to_fig_name, FolderOut, FileOutName + '_3_failed')
+            #except Exception:
+            #    pass
+
+
+FolderInPath = 'Q:\\DPE_andrej_data_output\\2023_05_16_TPX2_VdG\\X00\\04_200V\\Files\\'
+FolderOut = 'C:\\Users\\andrej\\Documents\\FEI\\data_processing_scripts\\figures_andrea\\'
+
+print(FolderInPath)
+print(FolderOut)
+filename_elist = 'ExtElist.txt'
+
+elist_path = FolderInPath + filename_elist
+elist_data = np.loadtxt(elist_path, skiprows=2, delimiter=';')
+clog = read_clog_multiple(FolderInPath)
+
+if len(elist_data[:,0]) == len(clog[:]):
+    print('Great! The Elist and Clog are of the same length', len(elist_data[:,0]), len(clog[:]))
+else:
+    print('Really bad! The Elist and Clog are NOT of the same length', len(elist_data[:,0]), len(clog[:]))
+
+E_min = np.array([100,150,200,250,300,350,400,450,500])
+E_max = 500000
+Size_min = np.array([10,20,30,40,50,60,70,80,90,100,125,150])
+Size_max = 1000
+Roundness_min = np.array([0.50,0.60,0.70,0.75,0.80,0.85,0.90,0.95,0.98,0.99])
+Roundness_max = 1.01
+
+number_of_particles = len(elist_data[:,0])
+
+#label_to_fig_name = '14.8 MeV neutrons'
+label_to_fig_name = '16.2 MeV neutrons'
+folder_name = '16_2MeV\\'
+
+for k in range(len(E_min)):
+    for l in range(len(Size_min)):
+        for j in range(len(Roundness_min)):
+            print(f'Filter range for Energy {E_min[k]}-{E_max} keV, size {Size_min[l]}-{Size_max} px, roundness {Roundness_min[j]}-{Roundness_max}.')
+            filter_parameters = Cluster_filter_multiple_parameter([E_min[k], E_max, Size_min[l], Size_max, Roundness_min[j], Roundness_max], [4,7,10]) # Energy, Size
+            filtered_elist = read_elist_filter_numpy(elist_data, filter_parameters)
+            square_matrices = create_matrix_filter_tpx3_t3pa_for_filtering_numpy_input(filtered_elist[:,-1], clog, number_of_particles)
+
+            energy_colorbar_max_value = 10000
+
+            FileOutName = str(label_to_fig_name) + '_E_' + str(E_min[k]) + '_Size_' + str(Size_min[l]) + '_Roundness_' + str(Roundness_min[j])
+
+            #try:
+            #    print_figure_energy(square_matrices[0], energy_colorbar_max_value, 'All - ' + label_to_fig_name, FolderOut, FileOutName + '_1_all')
+            #except Exception:
+            #    pass
+            try:
+                print_figure_energy(square_matrices[2], energy_colorbar_max_value, 'Passed - ' + label_to_fig_name, FolderOut + folder_name, FileOutName + '_2_passed')
+            except Exception:
+                pass
+            #try:
+            #    print_figure_energy(square_matrices[4], energy_colorbar_max_value, 'Failed - ' + label_to_fig_name, FolderOut, FileOutName + '_3_failed')
+            #except Exception:
+            #    pass
